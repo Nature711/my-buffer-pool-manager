ef6fd243f685beeea869557ad5c905dd02626c74
Nature711 <e0665488@u.nus.edu>
2023-06-11 10:18:03 +0800

    chores: add boilerplate code

---
+++ include/clock_replacer.h
@@ -0,0 +1,13 @@
+#ifndef CLOCK_REPLACER_H
+#define CLOCK_REPLACER_H
+
+class ClockReplacer
+{
+public:
+    // Constructor
+    ClockReplacer();
+
+    // Your code here
+};
+
+#endif // CLOCK_REPLACER_H
---
+++ src/page.cpp
@@ -0,0 +1,8 @@
+#include "buffer_pool_manager.h"
+
+Page::Page()
+{
+    // Initialize page
+}
+
+// Other function implementations of Page
